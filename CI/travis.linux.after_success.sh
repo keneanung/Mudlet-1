8dfe5fb85a85b6fe34ccc90065655fb1328ca4df
Keneanung <keneanung@googlemail.com>
2017-10-06 15:47:11 +0200

    Bugfix

--- CI/travis.linux.after_success.sh
+++ CI/travis.linux.after_success.sh
@@ -4,7 +4,7 @@ set -e
 
 # we deploy only qmake and gcc combination for linux
 if [ "${Q_OR_C_MAKE}" = "qmake" ] && [ "${CC}" = "gcc" ]; then
-  git clone --branchAddGstreamerPlugins https://github.com/keneanung/mudlet-installers.git "${TRAVIS_BUILD_DIR}/../installers"
+  git clone --branch=AddGstreamerPlugins https://github.com/keneanung/mudlet-installers.git "${TRAVIS_BUILD_DIR}/../installers"
 
   cd "${TRAVIS_BUILD_DIR}/../installers/generic-linux"
 
